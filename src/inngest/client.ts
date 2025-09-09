cb967e64fdef40440e8a76cfbf19f74e4d15e60c
unknown <ashish11to12@gmail.com>
2025-09-09 14:11:14 +0530

    Configured ingest to run on production from deployement

--- src/inngest/client.ts
+++ src/inngest/client.ts
@@ -3,6 +3,5 @@ import { env } from "~/env";
 
 export const inngest = new Inngest({
   id: "moontune",
-  // use a dummy key locally, real env var otherwise
-  eventKey: "dev_test_key",
+  eventKey: env.INNGEST_EVENT_KEY, 
 });
